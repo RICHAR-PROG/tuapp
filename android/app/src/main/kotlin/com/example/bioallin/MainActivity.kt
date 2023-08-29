6fc9aadcf1d42aa16562a2f8a7c05df06c9ea802
Richar-prog <2019141031@unh.edu.pe>
2023-08-28 19:30:24 -0500

    home

---
+++ android/app/src/main/kotlin/com/example/bioallin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bioallin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/screens/home_screen.dart
@@ -0,0 +1,32 @@
+import 'package:bioallin/presentation/widgets/home/home.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(2.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNlYc4hgIWCe8l-JD3V-39xTRk9YHYBd4mzDzoyP4rv47uZA_FZh6YcRKq_E63jU446I0&usqp=CAU"),
+          ),
+        ),
+        title: const Text("Welcome to Bioallin"),
+        centerTitle: false,
+      ),
+      // ignore: prefer_const_constructors
+      body: SingleChildScrollView(
+        child: const CustomPaint(
+          // painter: BottomNavCurvePainter(),
+          child: Home(),
+        ),
+      ),
+
+      // bottomNavigationBar: const BottomNavBarCurvedFb1(),
+    );
+  }
+}
