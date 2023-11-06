4433484a3d0f33c1b5c12178620eed24f870126b
Richar-prog <2019141031@unh.edu.pe>
2023-11-06 11:28:05 -0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 // import 'package:bioallin/presentation/screens/home_screen.dart';
-import 'package:bioallin/presentation/screens/map_screen.dart';
-import 'package:bioallin/presentation/screens/request_screen.dart';
-import 'package:bioallin/presentation/widgets/views/map.dart';
+import 'package:bioallin/presentation/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -22,6 +20,6 @@ class MyApp extends StatelessWidget {
             titleTextStyle: TextStyle(color: Colors.white),
           ),
         ),
-        home: const MapS());
+        home: const HomeScreen());
   }
 }
