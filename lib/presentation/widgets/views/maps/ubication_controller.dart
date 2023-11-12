e91b304a248d73e123e936d3ff373df3a6e200f1
edisonram <edisonlive2000@gmail.com>
2023-11-12 06:06:36 -0500

    use google maps

--- lib/presentation/widgets/views/maps/ubication_controller.dart
+++ lib/presentation/widgets/views/maps/ubication_controller.dart
@@ -1,11 +1,43 @@
-// TODO Implement this library.
+import 'dart:async';
 
+import 'package:flutter/widgets.dart' show ChangeNotifier;
 import 'package:bioallin/presentation/widgets/views/maps/utils/maps_style.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class UbicacionController {
+class UbicacionController extends ChangeNotifier {
+  final Map<MarkerId, Marker> _markers = {};
+  Set<Marker> get markers => _markers.values.toSet();
+  final _markersController = StreamController<String>.broadcast();
+  Stream<String> get onMarkerTap => _markersController.stream;
+
+  static const initialCameraPosition = CameraPosition(
+      target: LatLng(-12.39769625482686, -74.86838076508566), zoom: 16);
+
   // ignore: non_constant_identifier_names
   static void OnMapCreated(GoogleMapController controller) {
     controller.setMapStyle(mapStyle);
+  }
+
+  void onTap(LatLng position) {
+    final id = _markers.length.toString();
+    final markerId = MarkerId(_markers.length.toString());
+    final marker = Marker(
+        markerId: markerId,
+        position: position,
+        draggable: true,
+        onTap: () {
+          _markersController.sink.add(id);
+        },
+        onDragEnd: (newPosition) {
+          print("new position:  $newPosition");
+        });
+    _markers[markerId] = marker;
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    _markersController.close();
+    super.dispose();
   }
 }
